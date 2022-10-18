169cefd2cd8856a57e6791b78cafa95cfcb23f20
lionking0921 <111115936+lionking0921@users.noreply.github.com>
2022-10-18 22:18:20 +0800

    Update search.js

--- script/search.js
+++ script/search.js
@@ -1,4 +1,4 @@
- let value = ["吴君迪个人网站的运作原理是什么？","吴君迪100米最佳成绩是多少？", "吴君迪200米最佳成绩是多少？", "吴君迪骑车极速是多少？", "吴君迪跑步极速是多少？", "吴君迪最喜欢哪个品种的狗？", "吴君迪是哪个幼儿园毕业的？", "吴君迪去过鸟巢体育馆吗？", "吴君迪会骑马吗？", "吴君迪会哪些语言？", "吴君迪一口气能跑多远？", "吴君迪一口气能骑行多远？","如何下载吴君迪写的天气软件？"];
+ let value = ["【问题】吴君迪个人网站的运作原理是什么？","【问题】吴君迪100米最佳成绩是多少？", "【问题】吴君迪200米最佳成绩是多少？", "【问题】吴君迪骑车极速是多少？", "【问题】吴君迪跑步极速是多少？", "【问题】吴君迪最喜欢哪个品种的狗？", "【问题】吴君迪是哪个幼儿园毕业的？", "【问题】吴君迪去过鸟巢体育馆吗？", "【问题】吴君迪会骑马吗？", "【问题】吴君迪会哪些语言？", "【问题】吴君迪一口气能跑多远？", "【问题】吴君迪一口气能骑行多远？","【问题】如何下载吴君迪写的天气软件？"];
     let inp = document.querySelector("input");
     let neiron = document.querySelector(".neiron");
     inp.onkeyup=function(){
@@ -14,7 +14,6 @@ neiron.style.display = "none";
       value.forEach((item) => {
        
         let res = item.indexOf(inp.value);
-        console.log(res);
         if (res != -1) {
             // 这里判断就是把不是-1的读取出来
          
@@ -29,47 +28,47 @@ neiron.innerHTML=""
 }
 
       
-if(item=="吴君迪个人网站的运作原理是什么？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q1.html'>💡"+item+"</a><p></p></div>"
+if(item=="【问题】吴君迪个人网站的运作原理是什么？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q1.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪100米最佳成绩是多少？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q2.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪100米最佳成绩是多少？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q2.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪200米最佳成绩是多少？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q3.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪200米最佳成绩是多少？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q3.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪骑车极速是多少？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q4.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪骑车极速是多少？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q4.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪跑步极速是多少？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q5.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪跑步极速是多少？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q5.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪最喜欢哪个品种的狗？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q6.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪最喜欢哪个品种的狗？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q6.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪是哪个幼儿园毕业的？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q7.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪是哪个幼儿园毕业的？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q7.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪去过鸟巢体育馆吗？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q8.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪去过鸟巢体育馆吗？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q8.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪会骑马吗？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q9.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪会骑马吗？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q9.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪会哪些语言？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q10.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪会哪些语言？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q10.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪一口气能跑多远？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q11.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪一口气能跑多远？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q11.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="吴君迪一口气能骑行多远？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q12.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】吴君迪一口气能骑行多远？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q12.html'>❓"+item+"</a><p></p></div>"
 }
-else if(item=="如何下载吴君迪写的天气软件？"){
-neiron.innerHTML+="<div class='tip'><a href='/que/q13.html'>💡"+item+"</a><p></p></div>"
+else if(item=="【问题】如何下载吴君迪写的天气软件？"){
+neiron.innerHTML+="<div class='tip'><a href='/que/q13.html'>❓"+item+"</a><p></p></div>"
 }
 else{
-neiron.innerHTML+="<div class='tip'><p>😭糟糕，问题不见了</p></div>"
+neiron.innerHTML+="<div class='tip'><p>😭糟糕，内容不见了</p></div>"
 }
 
 
