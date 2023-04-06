c692db2468fecdafaf969cbbb340cfaa8afdd384
lionking0921 <111115936+lionking0921@users.noreply.github.com>
2023-04-06 21:51:32 +0800

    Add files via upload

--- script/calculate.js
+++ script/calculate.js
@@ -8,28 +8,36 @@ function main(){
     var dpowsum=0
     var xigamad=-1
     
-    for(var i=1;i<=10;i++){
-        if(document.getElementById("d"+i).value!=NaN&&document.getElementById("d"+i).value>0.0){
-            arrd.push(document.getElementById("d"+i).value)
+    var dlist=document.getElementById("d").value
+    dlist=dlist.replace(' ','')
+    if(dlist!=""){
+        arrd=dlist.split(",")
+        
+        for(var j=0;j<arrd.length;j++){
+            if(isNaN(arrd[j])||arrd[j]==""){
+                alert("请正确输入直径数据")
+                flag=0
+                break
+            }     
         }
     }
-    var dlen=arrd.length
 
-    if (dlen>0){
+    
+    var dlen=arrd.length
+    if (flag){
         for(var i=0;i<dlen;i++){
             dsum=dsum+parseFloat(arrd[i])
             }
             dba=dsum/dlen
-            
-            
             for(var i=0;i<dlen;i++){
             dpowsum=dpowsum+((parseFloat(arrd[i])-dba)*(parseFloat(arrd[i])-dba))
             }
-            xigamad=Math.sqrt(dpowsum/dlen)
-    }
-    else{
-        alert("请至少输入一项直径数据")
-        flag=0
+            if(dlen>1){
+                xigamad=Math.sqrt(dpowsum/(dlen*(dlen-1)))
+            }
+            else{
+                xigamad="无数据"
+            }
     }
 
 //计算时间t(s)的平均值和标准差
@@ -40,29 +48,39 @@ function main(){
     var tpowsum=0
     var xigamat=-1
     
-    for(var i=1;i<=10;i++){
-        if(document.getElementById("t"+i).value!=NaN&&document.getElementById("t"+i).value>0.0){
-            arrt.push(document.getElementById("t"+i).value)
+    var tlist=document.getElementById("t").value
+    tlist=tlist.replace(' ','')
+    if(tlist!=""){
+        arrt=tlist.split(",")
+        
+        for(var j=0;j<arrt.length;j++){
+            if(isNaN(arrt[j])||arrt[j]==""){
+                alert("请正确输入时间数据")
+                flag=0
+                break
+            }     
         }
     }
+    
+    
     var tlen=arrt.length
-    if(tlen>0){
+    if (flag){
         for(var i=0;i<tlen;i++){
             tsum=tsum+parseFloat(arrt[i])
             }
             tba=tsum/tlen
-            
-            
             for(var i=0;i<tlen;i++){
             tpowsum=tpowsum+((parseFloat(arrt[i])-tba)*(parseFloat(arrt[i])-tba))
             }
-            xigamat=Math.sqrt(tpowsum/tlen)
+            if(tlen>1){
+                xigamat=Math.sqrt(tpowsum/(tlen*(tlen-1)))
+            }
+            else{
+                xigamat="无数据"
+            }
     }
-    else{
-        alert("请至少输入一项时间数据")
-        flag=0
-    }
-//计算粘度系数t(Pa·s)的平均值和标准差
+
+//计算粘度系数η(Pa·s)的平均值和标准差
     var p=-1
     var p0=-1
     var od=-1
@@ -84,31 +102,27 @@ function main(){
     H=parseFloat(document.getElementById("H").value)
 
     if(p!=NaN&&p>0.0&&p0!=NaN&&p0>0.0&&D0!=NaN&&D0>0.0&&L!=NaN&&L>0.0&&H!=NaN&&H>0.0){
-    for (var i=1;i<=10;i++){
-        if(document.getElementById("d"+i).value!=NaN&&document.getElementById("d"+i).value>0.0&&document.getElementById("t"+i).value!=NaN&&document.getElementById("t"+i).value>0.0){
-             od=parseFloat(document.getElementById("d"+i).value)
-             t=parseFloat(document.getElementById("t"+i).value)
-             arreta.push(etabal(p,p0,od,t,L,D0,H))
-             datanum++
+    if(dlen>0&&tlen>0&&flag>0){
+        minl=dlen<tlen?dlen:tlen
+        for (var i=0;i<minl;i++){
+                 od=parseFloat(arrd[i])
+                 t=parseFloat(arrt[i])
+                 arreta.push(etabal(p,p0,od,t,L,D0,H))
+                 datanum++
         }
-        else{
-            break
-        }
-    }
-    if(arreta.length>0){
         for(var i=0;i<arreta.length;i++){
-            etasum=etasum+parseFloat(arreta[i])
+                etasum=etasum+parseFloat(arreta[i])
         }
-        etaba=etasum/arreta.length
+            etaba=etasum/arreta.length
         for(var i=0;i<arreta.length;i++){
             etapowsum=etapowsum+((parseFloat(arreta[i])-etaba)*(parseFloat(arreta[i])-etaba))
         }
         xigamaeta=Math.sqrt(etapowsum/arreta.length)
+
     }
     else{
-        alert("输入的直径和时间数据没有对齐")
         flag=0
-    } 
+    }    
     }
     else{
         alert("必填项不能为空")
@@ -129,12 +143,12 @@ function main(){
 
     var useta=-1
     
-    usl=parseFloat(document.getElementById("deltaL").value)
-    usd=parseFloat(document.getElementById("deltad").value)
-    ust=parseFloat(document.getElementById("deltat").value)
-    usp=parseFloat(document.getElementById("deltap").value)
+    usl=parseFloat(document.getElementById("deltaL").value)/2
+    usd=parseFloat(document.getElementById("deltad").value)/2
+    ust=parseFloat(document.getElementById("deltat").value)/2
+    usp=parseFloat(document.getElementById("deltap").value)/2
     
-    if(usl!=NaN&&usl>0.0&&usd!=NaN&&usd>0.0&&ust!=NaN&&ust>0.0&&usp!=NaN&&usp>0.0&&H!=NaN&&H>0.0){
+    if(usl!=NaN&&usl>0.0&&usd!=NaN&&usd>0.0&&ust!=NaN&&ust>0.0&&usp!=NaN&&usp>0.0&&H!=NaN&&H>0.0&&p>p0){
         Usp0=usp/Math.sqrt(3)
         UsL=usl/Math.sqrt(3)
         Ust=Math.sqrt(xigamat+(ust/Math.sqrt(3)))
@@ -143,7 +157,11 @@ function main(){
         useta=E*etaba
     }
     else{
-        E="未填写"
+        if(p<=p0){alert("提示：小球密度必须大于液体密度")}
+        E="无数据"
+        Usd="无数据"
+        Ust="无数据"
+        useta="无数据"
     }
 //计算小球速度v
     var v=-1
@@ -159,11 +177,11 @@ if(flag){
     }
 
     document.getElementById("dba").innerHTML="小球直径平均值d="+dba+"mm"
-    document.getElementById("usad").innerHTML="直径A类不确定度UsAd=标准差σd="+xigamad+"mm"
+    document.getElementById("usad").innerHTML="直径A类不确定度UsAd=标准误差σd="+xigamad+"mm"
     document.getElementById("usbd").innerHTML="直径B类不确定度UsBd="+Usd+"mm"
 
     document.getElementById("tba").innerHTML="时间平均值t="+tba+"s"
-    document.getElementById("usat").innerHTML="时间A类不确定度UsAt=标准差σt="+xigamat+"s"
+    document.getElementById("usat").innerHTML="时间A类不确定度UsAt=标准误差σt="+xigamat+"s"
     document.getElementById("usbt").innerHTML="时间B类不确定度UsBt="+Ust+"s"
 
     document.getElementById("etaba").innerHTML="平均粘度系数η="+etaba+"Pa·s"
